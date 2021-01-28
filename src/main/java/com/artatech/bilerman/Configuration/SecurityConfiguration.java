dbf8af7aaf625c3253c76296b697c28cd9441a3e
Talgar <talguulu@a483e789ac88.ant.amazon.com>
2021-01-28 20:17:35 +0100

    familiar tags as public

--- src/main/java/com/artatech/bilerman/Configuration/SecurityConfiguration.java
+++ src/main/java/com/artatech/bilerman/Configuration/SecurityConfiguration.java
@@ -74,7 +74,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .antMatchers("/api/user/checkUsernameAvailability", "/api/user/checkEmailAvailability")
                 .permitAll()
-                .antMatchers(HttpMethod.GET, "/api/polls/**", "/api/users/**", "/api/articles/**", "/api/comments/**")
+                .antMatchers(HttpMethod.GET, "/api/polls/**", "/api/users/**", "/api/articles/**", "/api/comments/**", "/api/tags/**")
                 .permitAll()
                 .anyRequest().authenticated();
 
